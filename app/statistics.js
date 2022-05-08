57275ce86706ef9180694d97dc5237a247b8c4cc
JagadishNM <91608364+JagadishNM@users.noreply.github.com>
2022-05-08 11:43:31 +0530

    Update statistics.js

--- app/statistics.js
+++ app/statistics.js
@@ -1,5 +1,34 @@
-module.exports=function getStatistics(numbers){
-    
-    //implement the computation of statistics here
+module.exports = function getStatistics(numbers) {
+
+    let result = {};
+
+    //reportsNaNForEmptyInput
+    if (numbers.length === 0) {
+        result.average=Number.NaN;
+        result.max = Number.NaN;
+        result.min = Number.NaN;
+        return result;
+    }
+
+    // reportsAverageMinMaxx
+    let max = Number.NEGATIVE_INFINITY;
+    let min = Number.POSITIVE_INFINITY;
+    let avg = 0;
+
+    for (let i = 0; i < numbers.length; i++) {
+        if (numbers[i] > max) {
+            max = numbers[i];
+        }
+        if (numbers[i] < min) {
+            min = numbers[i];
+        }
+        avg += numbers[i];
+    }
+
+    result.min = min;
+    result.max = max;
+    result.average = (avg / numbers.length);
+    return result;
+
 }
 
